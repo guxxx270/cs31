a38736e996d5804560519811c342a0da5b437804
Quanju Gu <quanjugu@Quanjus-MacBook-Pro.local>
2016-08-20 16:30:31 +0800

    xxxxx

--- project2/tax.cpp
+++ project2/tax.cpp
@@ -6,7 +6,54 @@
 //  Copyright © 2016 Jerry Gu. All rights reserved.
 //
 #include <iostream>
-#include <string>   //string...
-#include <cstdlib> //rand() and exit function
+//#include <string>   //string...
+//#include <cstdlib> //rand() and exit function
 
 using namespace std;
+
+int main()
+{
+  int tax;
+
+  //Get the Market value
+  cout << "Market value (in millions):";
+  int Market_value;
+  cin >> Market_value;
+  //cout << Market_value << endl;
+  cin.ignore(10000, '\n');//this line is important
+
+  //Get the Indentification imfornation
+  cout << "Identification:";
+  string identification;
+  //cin >> identification;//different of cin and getline. cin is end in space so CAN'T use cin here
+  getline(cin, identification);
+  //cout << identification << endl;
+
+  //Get the State imfornation
+  cout << "State:";
+  string state;
+  getline(cin, state);
+  //cout << state << endl;
+
+
+
+  //print the result part.
+  cout << "---" << endl;
+  cout << "The tax for " << identification << " is "  << tax << " million." << endl;
+
+//The reason for using the cin.ignore
+/*
+int a,b,c;
+cout<<"input a:";
+cin>>a;
+cin.ignore(1024, '\n');
+cout<<"input b:";
+cin>>b;
+cin.ignore(1024, '\n');
+cout<<"input c:";
+cin>>c;
+cout<<a<<"\t"<<b<<"\t"<<c<<endl;
+*/
+
+
+}
