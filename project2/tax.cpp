0f9c5cd380f8a5c262e4edca2711cd0c93d16fe7
Quanju Gu <quanjugu@Quanjus-MacBook-Pro.local>
2016-08-22 21:45:58 +0800

    xxxx

--- project2/tax.cpp
+++ project2/tax.cpp
@@ -6,25 +6,33 @@
 //  Copyright © 2016 Jerry Gu. All rights reserved.
 //
 #include <iostream>
-//#include <string>   //string...
+#include <string>   //string...
 //#include <cstdlib> //rand() and exit function
 
 using namespace std;
 
 int main()
 {
-  int tax;
+  double tax;
+  const double level_200 = 200;
+  const double level_300 = 300;
+  const double tax_200 = 4.1/100;
+  cout << tax_200;
 
   //Get the Market value
   cout << "Market value (in millions):";
-  int Market_value;
+  double Market_value;
   cin >> Market_value;
   if (Market_value < 0)
   {
+    cout << "---" << endl;
     cout << "The market value must be nonnegative." << endl;
-    return 0;
+    return 1;
   }
-  //cout << Market_value << endl;
+  cout.setf(ios::fixed);//This two line make sure to show in 3232.043 style
+  cout.precision(3);
+
+  cout << Market_value << endl;
   cin.ignore(10000, '\n');//this line is important
 
   //Get the Indentification imfornation
@@ -33,18 +41,28 @@ int main()
   //cin >> identification;//different of cin and getline. cin is end in space so CAN'T use cin here
   getline(cin, identification);
   //cout << identification << endl;
-
+  if (identification == "")
+  {
+    cout << "---" << endl;
+    cout << "You must enter a property identification." << endl;
+    return 2;
+  }
   //Get the State imfornation
   cout << "State:";
   string state;
   getline(cin, state);
   //cout << state << endl;
-
+  if (state == "")
+  {
+    cout << "---" << endl;
+    cout << "You must enter a state." << endl;
+    return 3;
+  }
 
 
   //print the result part.
   cout << "---" << endl;
-  cout << "The tax for " << identification << " is "  << tax << " million." << endl;
+  cout << "The tax for " << identification << " is $"  << tax << " million." << endl;
 
 //The reason for using the cin.ignore
 /*
