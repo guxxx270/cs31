05f18ace663cf1ffde960430b277dca3f6c19452
Quanju Gu <quanjugu@Quanjus-MacBook-Pro.local>
2016-08-28 11:03:58 +0800

    I/O test

--- project5/iotest.cpp
+++ project5/iotest.cpp
@@ -16,4 +16,17 @@ int main()
   }
   outfile << "This will be written to the file" << endl;
   outfile << "2 + 2 =" << 2+2 << endl;
+
+  ifstream infile("data.txt");
+  if(!infile)
+  {
+    cerr << "Error: Cannot open data.txt!" << endl;
+  }
+
+  int k;
+  cout << k << endl;
+  infile >> k;
+  infile.ignore(10000, '\n');
+  cout << k << endl;
+
 }
